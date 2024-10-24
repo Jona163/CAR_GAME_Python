39944bf46f0adbbfa2fbda16730840794a812507
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-24 14:19:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ green = (76, 208, 56)
 red = (200, 0, 0)
 white = (255, 255, 255)
 yellow = (255, 232, 0)
+
+# Ajustes del stteo
+clock = pygame.time.Clock()
+fps = 120
+
+# Ajustes del juego principal
+gameover = False
+speed = 2
+score = 0
