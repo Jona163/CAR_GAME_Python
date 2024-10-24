c0db7534b7677e678e04fd24b3e3aaa6db316210
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-24 14:18:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from pygame.locals import *
 import random
 
 pygame.init()
+
+# Creación de la ventana
+width = 800
+height = 600
+screen_size = (width, height)
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('MODELADO')
