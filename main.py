17649d6f6d07706b77014a25de728c7ac1f6da91
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-25 17:48:31 -0600

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#Jona163# Autor: Jonathan Hernández
+#Jona163
+# Autor: Jonathan Hernández
 # Fecha: 17 octubre 2024
 # Descripción: Calculadora Vectorial en Python Version 2.
 # GitHub: https://github.com/Jona163
